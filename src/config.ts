29a51d0b3f447a34102e03a9cf2ca44a6df8b0a7
Yegor Zhartun <egorzhartun@gmail.com>
2025-01-30 00:40:40 +0300

    some changes

---
+++ src/config.ts
@@ -0,0 +1,4 @@
+export const SITE_TITLE = 'ezh / portfolio';
+export const SITE_DESCRIPTION = 'full-stack developer capable of creating and maintaining complex web applications, with deep knowledge in both frontend and backend technologies.';
+export const GENERATE_SLUG_FROM_TITLE = true
+export const TRANSITION_API = true
---
+++ src/data/info.ts
@@ -0,0 +1,76 @@
+export const info = {
+    baseUrl: "/",
+    name: "E Z H",
+    jobDescription: "--",
+    about: `--
+    `,
+  
+    experience: [
+      {
+        name: "--",
+        location: "--",
+        startDate: "--",
+        endDate: "--",
+        description: [
+          "--",
+          "--",
+        ],
+      },
+  
+      {
+        name: "--",
+        location: "--",
+        startDate: "--",
+        endDate: "--",
+        description: [
+          "--",
+          "--",
+          "--",
+          "--",
+          "--",
+          "--",
+        ],
+      },
+  
+      {
+        name: "--",
+        location: "--",
+        startDate: "--",
+        endDate: "--",
+        description: [
+          "--",
+          "--",
+        ],
+      },
+    ],
+  
+    education: [
+      {
+        name: "--",
+        location: "--",
+        startDate: "--",
+        endDate: "--",
+        description: [
+          "--",
+        ],
+      },
+    ],
+  
+    socialMedia: {
+      facebook: "--",
+      twitter: "--",
+      github: "--",
+      email: "--",
+      linkedin: "--",
+    },
+  
+    projects: [
+      {
+        title: "--",
+        isFeatured: true,
+        thumbnail: "--",
+        githubUrl: "--",
+        liveUrl: "--",
+      },
+    ],
+  };
