64e07796240de7cebc277be587bdf9bee254048d
Yegor Zhartun <egorzhartun@gmail.com>
2025-01-30 09:01:44 +0300

    some changes have been made

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,75 +1,69 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			animation: {
-				typewriter: 'typewriter 2s steps(11) forwards',
-				caret: 'typewriter 2s steps(11) forwards, blink 1s steps(11) infinite 2s',
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      animation: {
+        typewriter: "typewriter 2s steps(11) forwards",
+        caret:
+          "typewriter 2s steps(11) forwards, blink 1s steps(11) infinite 2s",
 
+        fadeIn: "fadeIn 2s ease-in forwards",
+        fadeOut: "fadeOut 2s ease-out",
+      },
+      keyframes: {
+        fadeIn: {
+          "0%": { opacity: "0" },
+          "100%": { opacity: "1" },
+        },
 
-				fadeIn: 'fadeIn 2s ease-in',
-				fadeOut: 'fadeOut 2s ease-out'
-			},
-			keyframes: {
+        fadeOut: {
+          "0%": {
+            opacity: "1",
+          },
+          "100%": {
+            opacity: "0",
+          },
+        },
 
-				
-				fadeIn: {
-					'0%': {
-						opacity: '0',
-					},
-					'100%': {
-						opacity: '1'
-					}
-				},
-				
-				fadeOut: {
-					'0%': {
-						opacity: '1'
-					},
-					'100%': {
-						opacity: '0'
-					}
-				},
+        typewriter: {
+          to: {
+            left: "100%",
+          },
+        },
 
-				typewriter: {
-					to: {
-						left: '100%',
-					},
-				},
-
-				blink: {
-					'0%': {
-						opacity: '0',
-					},
-					'0.1%': {
-						opacity: '1',
-					},
-					'50%': {
-						opacity: '1',
-					},
-					'50.1%': {
-						opacity: '0',
-					},
-					'100%': {
-						opacity: '0',
-					},
-				},
-			},	
-			colors: {
-				//light: "#F4F5F0",
-				light: "#888A7E",
-				dark: "#111111",
-				//light: "#f3f3f3",
-				blue: "#33d2ff",
-				gray: "#C4C4C4",
-				"dark-gray": "#1A1A1A",
-			},
-				fontFamily: {
-				inter: ["Inter", "sans-serif"],
-				outfit: ["Outfit", "sans-serif"],
-			},
-		},
-	},
-	plugins: [],
-}
+        blink: {
+          "0%": {
+            opacity: "0",
+          },
+          "0.1%": {
+            opacity: "1",
+          },
+          "50%": {
+            opacity: "1",
+          },
+          "50.1%": {
+            opacity: "0",
+          },
+          "100%": {
+            opacity: "0",
+          },
+        },
+      },
+      colors: {
+        //light: "#F4F5F0",
+        light: "#888A7E",
+        dark: "#111111",
+        //light: "#f3f3f3",
+        blue: "#33d2ff",
+        gray: "#C4C4C4",
+        "dark-gray": "#1A1A1A",
+      },
+      fontFamily: {
+        inter: ["Inter", "sans-serif"],
+        outfit: ["Outfit", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
